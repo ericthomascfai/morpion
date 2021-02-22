3aa4601cadc43b37c74d9921dc44bde1ad0ac525
eric <eric.thomas@formation-industries-fc.fr>
2021-02-22 09:47:13 +0100

    prototypes fonctions

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 
 void affichejeu(); //affiche la grille
-void initjeu(); //remplissage a blanc
+void initjeu(); //remplissage à blanc de la grille
 bool verificationcoup(int l,int c); //vérification si le coup est valide
 void jouer(); //saisie du coup à jouer
-char determinesymbole(); //determine le symbole qui va être joueur
+char determinesymbole(); //determine le symbole qui va être joué
 bool verifgagneligne(); //vérification si une ligne est gagnante
 bool verifgagnecolonne(); //vérification si une colonne est gagnante
 bool verifgagnediagonale(); //vérification si une diagonale est gagnante
