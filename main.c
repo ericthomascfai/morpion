f27007889036338dd278b26126be2aaed5e0964c
eric <eric.thomas@formation-industries-fc.fr>
2021-02-22 09:45:18 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+void affichejeu(); //affiche la grille
+void initjeu(); //remplissage a blanc
+bool verificationcoup(int l,int c); //vérification si le coup est valide
+void jouer(); //saisie du coup à jouer
+char determinesymbole(); //determine le symbole qui va être joueur
+bool verifgagneligne(); //vérification si une ligne est gagnante
+bool verifgagnecolonne(); //vérification si une colonne est gagnante
+bool verifgagnediagonale(); //vérification si une diagonale est gagnante
+
+
+int main() {
+//TO DO
+    return 0;
+}
